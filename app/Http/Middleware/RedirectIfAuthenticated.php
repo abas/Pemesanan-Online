6fe2cede61c3383e55743de9dcec0703d6d3c365
ahmadbasir <akhmadbasir5@gmail.com>
2017-12-21 10:58:24 +0700

    authorize

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return redirect('/home')->with('status','You are Logon');
         }
 
         return $next($request);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,28 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        @if (session('status'))
+            <div class="col-md-12">
+            
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            </div>
+        @endif
+        <div class="col-md-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Menu</div>
+                <div class="panel-body">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-10">
+            <div class="panel panel-default">
+                <div class="panel-heading">Post</div>
 
                 <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    
                 </div>
             </div>
         </div>
