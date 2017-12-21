b08f2be01e893e5a8712d6100adbeda76d3231d9
ahmadbasir <akhmadbasir5@gmail.com>
2017-12-21 10:27:38 +0700

    update register blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,7 +15,7 @@
                             <label for="name" class="col-md-4 control-label">Name</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <input placeholder="your name " id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
                                 @if ($errors->has('name'))
                                     <span class="help-block">
@@ -29,11 +29,53 @@
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                <input placeholder="ex: username@mail.com" id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        
+                        <div class="form-group{{ $errors->has('kontak') ? ' has-error' : '' }}">
+                            <label for="kontak" class="col-md-4 control-label">Kontak Person</label>
+
+                            <div class="col-md-6">
+                                <input placeholder="ex: 081234xxx or @telegram @line @instagram" id="kontak" type="kontak" class="form-control" name="kontak" value="{{ old('kontak') }}" required>
+
+                                @if ($errors->has('kontak'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('kontak') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        
+                        <div class="form-group{{ $errors->has('no_rek') ? ' has-error' : '' }}">
+                            <label for="no_rek" class="col-md-4 control-label">Atm</label>
+
+                            <div class="col-md-6">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <select name="name_rek" id="name_rek" class="form-control">
+                                            <option >...</option>
+                                            <option value="mandiri">Mandiri</option>
+                                            <option value="bri">BRI</option>
+                                            <option value="bni">BNI</option>
+                                            <option value="jateng">Jateng</option>
+                                        <!-- <input id="no_rek" type="no_rek" class="form-control" name="no_rek" value="{{ old('no_rek') }}" required> -->
+                                        </select>                                
+                                    </div>
+                                    <div class="col-md-8">                                
+                                        <input id="no_rek" type="no_rek" class="form-control" name="no_rek" value="{{ old('no_rek') }}" required>
+                                    </div>
+                                </div>
+
+                                @if ($errors->has('no_rek'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('no_rek') }}</strong>
                                     </span>
                                 @endif
                             </div>
