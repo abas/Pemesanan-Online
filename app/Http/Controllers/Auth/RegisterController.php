38174c4a89c7c58501300b0b01ea170b1817431c
ahmadbasir <akhmadbasir5@gmail.com>
2017-12-21 10:18:35 +0700

    register controller update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,6 +51,8 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'kontak' => 'required|string|max:20',
+            'no_rek' => 'required|string|max:255',
         ]);
     }
 
@@ -66,6 +68,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'kontak' => $data['kontak'],
+            'no_rek' => $data['no_rek'],
         ]);
     }
 }
