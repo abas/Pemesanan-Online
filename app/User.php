fc6487b53f90cfa601631966c56fd98c5e12748f
ahmadbasir <akhmadbasir5@gmail.com>
2017-12-22 14:44:25 +0700

    update user model

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public static function getName($id)
+    {
+        return User::find($id)->name;
+    }
 }
